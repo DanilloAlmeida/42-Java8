64928da5080b2d96c156c5f45f6ba46060e870f7
Danillo <danillo.almeida@bb.com.br>
2022-12-22 17:07:40 -0300

    atualizando susebb 22/12/2022 17h07

--- Java*/src/OrdenaString.java
+++ Java*/src/OrdenaString.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class OrdenaString {
 
@@ -11,8 +13,39 @@ public class OrdenaString {
 		palavras.add("caelum");
 		
 		Collections.sort(palavras);
+		System.out.println("\ncomparando com o Collections.sort");
 		System.out.println(palavras);
 
-	}
+		System.out.println("\ncomparando com o comparator");
+		Comparator <String> comparador = new comparandoPorTamanho();
+		Collections.sort(palavras, comparador);
+		System.out.println(palavras);
 
+		System.out.println("\nimprimindo com o forEach");
+		Consumer<String> consumidor = new ImprimeNaLinha();
+		palavras.forEach(consumidor);
+//		System.out.println(palavras);
+	}
+}
+
+class ImprimeNaLinha implements Consumer<String>{
+
+	@Override
+	public void accept(String t) {
+		System.out.println(t);
+		
+	}
+	
+}
+
+class comparandoPorTamanho implements Comparator<String> {
+	
+	@Override
+	public int compare(String o1, String o2) {
+		if (o1.length() < o2.length())
+			return -1;
+		if (o1.length() > o2.length())
+			return 1;			
+		return 0;
+	}
 }
