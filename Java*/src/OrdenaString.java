8ff3381ac661734f4bd542386ac47c0a85ada60c
Danillo <danillo.almeida@bb.com.br>
2022-12-22 17:31:12 -0300

    atualizando susbb 17h31

--- Java*/src/OrdenaString.java
+++ Java*/src/OrdenaString.java
@@ -15,31 +15,57 @@ public class OrdenaString {
 		Collections.sort(palavras);
 		System.out.println("\ncomparando com o Collections.sort");
 		System.out.println(palavras);
-
+//----------------------------------------------------------------------		
 		System.out.println("\ncomparando com o comparator");
 		Comparator <String> comparador = new comparandoPorTamanho();
 		Collections.sort(palavras, comparador);
 		System.out.println(palavras);
-
-		System.out.println("\nimprimindo com o forEach");
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
+//----------------------------------------------------------------------		
+//		System.out.println("\nimprimindo com o forEach");
+//		Consumer<String> consumidor = new ImprimeNaLinha();
+//		palavras.forEach(consumidor);
 //		System.out.println(palavras);
+//		Consumer<String> consumidor = new Consumer<String>() {
+//			@Override
+//			public void accept(String t) {
+//				System.out.println(t);			
+//			}
+//		};
+//----------------------------------------------------------------------		
+		System.out.println("\nimprimindo com o Consumer");
+		Consumer<String> consumidor = new Consumer<String>() {
+			@Override
+			public void accept(String t) {
+				System.out.println(t);			
+			}
+		};
+		palavras.forEach(consumidor);
+//----------------------------------------------------------------------		
+		System.out.println("\nimprimindo com o Consumer embutido");
+//		Consumer<String> consumidor2 = 
+		palavras.forEach(new Consumer<String>() {
+			@Override
+			public void accept(String t) {
+				System.out.println(t);			
+			}
+		});
+//----------------------------------------------------------------------		
+		System.out.println("\nimprimindo com o Consumer com lambda");
+//		Consumer<String> consumidor2 = 
+		palavras.forEach((String t) -> {
+				System.out.println(t);
+		});
+//----------------------------------------------------------------------		
 	}
+//class ImprimeNaLinha implements Consumer<String>{
+//
+//	@Override
+//	public void accept(String t) {
+//		System.out.println(t);
+//		
+//	}	
 }
-
-class ImprimeNaLinha implements Consumer<String>{
-
-	@Override
-	public void accept(String t) {
-		System.out.println(t);
-		
-	}
-	
-}
-
 class comparandoPorTamanho implements Comparator<String> {
-	
 	@Override
 	public int compare(String o1, String o2) {
 		if (o1.length() < o2.length())
